e2506dadf813e8fc19d3635d928214d122533987
NikolaiSurodin <kolya_12.93@mail.ru>
2021-08-17 21:47:25 +0700

    added operators and styles

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div id="app">
     <router-view/>
   </div>
 </template>
@@ -13,5 +13,7 @@ export default {
 </script>
 
 <style>
-
+#app{
+  background-color: #fcd407;
+}
 </style>
